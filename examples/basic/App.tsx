6bbc3e9513627eeecf9e60be870a8b716c40f9ed
Anton Pawlik <git+noreply@antpaw.org>
2024-02-17 13:00:45 +0100

    reduce examples css

--- examples/basic/App.tsx
+++ examples/basic/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import reactLogo from "./assets/react.svg";
+import "./index.css";
 
 function App() {
 	const [count, setCount] = useState(0);
